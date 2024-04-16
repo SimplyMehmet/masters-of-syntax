1f6ea478b691d148597c1aee6bc0aa1d516e7f48
mgonen@harlemnext.com <mgonen@harlemnext.com>
2024-04-16 13:02:02 +0200

    added logo's and favicons

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    head: {
+      link: [
+        { rel: "manifest", href: "/favicon/site.webmanifest" },
+        { rel: "icon", href: "/favicon/favicon-16x16.png" },
+        { rel: "icon", href: "/favicon/favicon-32x32.png" },
+        { rel: "apple-touch-icon", href: "/favicon/apple-touch-icon.png" },
+      ]
+    }
+  },
   devtools: { enabled: true },
   vite: {
     server: {
