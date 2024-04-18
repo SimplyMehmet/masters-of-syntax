0b07eb51fa619d0cc39734cd039d7e57439ef659
mehmet gonen <mehmet@mastersofsyntax.nl>
2024-04-18 18:24:48 +0200

    add 1s delay

--- components/index/banner.vue
+++ components/index/banner.vue
@@ -3,10 +3,10 @@
         <div class="col-start-1 lg:col-start-7 col-end-13 text-white">
             <div class="flex h-lvh justify-center items-center">
                 <div class="logo-text font-700 text-7xl lg:mb-0 lg:line-height-70px lg:text-7xl xl:line-height-90px xl:text-8xl mb-40">
-                    <span class="opacity-0 animate-[fade-top_.6s_ease-out_forwards] block text-center">Masters</span>
-                    <span class="opacity-0 animate-[fade-bottom_.6s_ease-out_forwards] lg:animate-[fade-left.4s_ease-out_forwards] inline-block">of&nbsp;</span>
-                    <span class="opacity-0 animate-[fade-bottom_.6s_ease-out_forwards] inline-block">Syntax</span>
-                    <span class="block bg-white animate-[expand-width_.4s_.5s_ease-in-out_forwards] w-0 h-2 mt-3 lg:mt-6"></span>
+                    <span class="opacity-0 animate-[fade-top_.6s_1s_ease-out_forwards] block text-center">Masters</span>
+                    <span class="opacity-0 animate-[fade-bottom_.6s_1s_ease-out_forwards] lg:animate-[fade-left.4s_1s_ease-out_forwards] inline-block">of&nbsp;</span>
+                    <span class="opacity-0 animate-[fade-bottom_.6s_1s_ease-out_forwards] inline-block">Syntax</span>
+                    <span class="block bg-white animate-[expand-width_.4s_1.5s_ease-in-out_forwards] w-0 h-2 mt-3 lg:mt-6"></span>
                 </div>
             </div>
         </div>
