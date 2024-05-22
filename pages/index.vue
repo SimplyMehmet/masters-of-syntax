1049ebd084a9dc3f2b98c72192edad50d56c6e71
mehmet gonen <mehmet@mastersofsyntax.nl>
2024-05-22 11:45:35 +0200

    reset the design keep particles

--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,3 @@
 <template>
-  <div>
-    <div
-      class="pos-relative flex justify-end items-center h-lvh overflow-hidden"
-    >
-      <Particles repulse-selector="#connecting"></Particles>
-
-      <div
-        id="connecting"
-        class="h-2xl w-2xl flex items-center justify-center mb-40 lg:mb-0"
-      >
-        <IndexBanner />
-      </div>
-    </div>
-  </div>
+  <h1>Some text!</h1>
 </template>
